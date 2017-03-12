345ab6d12cab63852ccd7544772a53c336a93ce3
zitongliu <zitongliu7@gmail.com>
2017-03-12 15:36:00 +1100

    Completed Geolocation

---
+++ 21-Geolocation/js/main.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition((data)=> {
+  console.log(data);
+
+  speed.textContent = data.coords.speed;
+  arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+}, (err) => {
+  console.err(err);
+  alert('HEY! YOU GOTTA ALLOW THAT TO HAPPEN!!!');
+});
